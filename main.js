7fdc430f8cd65d29fb4d4d2ddaedd6f0476ff69e
kaustubh-khandwal <76221706+kaustubh-khandwal@users.noreply.github.com>
2021-05-07 19:42:01 +0530

    Car racing vroooooooooooooooooooooooooooooooooom

--- main.js
+++ main.js
@@ -10,6 +10,14 @@ car1X = 10;
 car1Y = 10;
 
 
+car2_width = 140;
+car2_height = 90;
+car2image = "car2.png";
+car2X = 10;
+car2Y = 120;
+
+
+
 background_imagetag = new Image();
 car1imagetag = new Image();
 car2imagetag = new Image();
@@ -21,6 +29,9 @@ function add() {
     
     car1imagetag.onload = uploadcar1;
     car1imagetag.src = car1image;
+
+    car2imagetag.onload = uploadcar1;
+    car2imagetag.src = car2image;
 }
 
 
@@ -30,6 +41,7 @@ function uploadbackground() {
 
 function uploadcar1() {
     ctx.drawImage(car1imagetag, car1X, car1Y, car1_width, car1_height);
+    ctx.drawImage(car2imagetag, car2X, car2Y, car2_width, car2_height);
 }
 
 
@@ -53,6 +65,39 @@ function my_keydown(e) {
     if (keyPressed == '39') {
         right();
         console.log("right");
+    }
+
+
+
+
+
+    if (keyPressed == '87') {
+        up2();
+        console.log("up");
+    }
+    if (keyPressed == '83') {
+        down2();
+        console.log("down");
+    }
+    if (keyPressed == '65') {
+        left2();
+        console.log("left");
+    }
+    if (keyPressed == '68') {
+        right2();
+        console.log("right");
+    }
+
+
+
+
+
+    if (car1X > 700) {
+        document.getElementById("game_status").innerHTML = "Car 1 won!!";
+    }
+
+    if (car2X > 700) {
+        document.getElementById("game_status").innerHTML = "Car 2 won!!";
     }
 }
 
@@ -84,6 +129,41 @@ function left() {
 function right() {
     if (car1X <= 700) {
         car1X += 10 ;
+        uploadbackground();
+        uploadcar1();
+        
+    }
+}
+
+
+
+function up2() {
+    if (car2Y >= 0) {
+        car2Y-= 10;
+        uploadbackground();
+        uploadcar1();
+    }
+}
+
+function down2() {
+    if (car2Y <= 500) {
+        car2Y += 10;
+        uploadbackground();
+        uploadcar1();
+    }
+}
+
+function left2() {
+    if (car2X >= 0) {
+        car2X -= 10;
+        uploadbackground();
+        uploadcar1();
+    }
+}
+
+function right2() {
+    if (car2X <= 700) {
+        car2X += 10 ;
         uploadbackground();
         uploadcar1();
         
